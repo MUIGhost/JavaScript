95fcc4cf61baec203774cf79ca7f942bef6fecbe
MUIGhost <167474710+MUIGhost@users.noreply.github.com>
2025-07-04 07:58:24 +0000

    Operations Done

---
+++ 01_Basics/operations.js
@@ -0,0 +1,54 @@
+//  OPERATIONS
+
+let value = 4
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2**3); //prints 2 to the power 3
+//console.log(2%3); // prints the remainder 
+
+let str1 = "Hi"
+let str2 = " Mom"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2"); // 12
+// console.log("1" + 2); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((3 + 4) * 5 % 3); // 2
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);
+
+// Postfix precedence
+//In this the value is used first and then incremented
+let a = 3
+const b = a++
+console.log(a,b); // 4 3
+
+// Prefix precedence
+// In this the value is first incremened and the used 
+let x = 3
+const y = ++x
+console.log(x,y); // 4 4
+
+
+
+
+
+
+
+
+
+
